eb27b239440d6dad4a744e7f2e223dba51b10c2d
memokey <skytopground@gmail.com>
2025-02-24 21:58:54 -0500

    Updated

--- index.ts
+++ index.ts
@@ -25,6 +25,22 @@ app.use(
 );
 app.use(express.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  )
+  if (req.method === "OPTIONS") {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "POST, PUT, PATCH, GET, DELETE"
+    )
+    return res.status(200).json({})
+  }
+  next()
+})
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Server is running... 🏃");
 });
