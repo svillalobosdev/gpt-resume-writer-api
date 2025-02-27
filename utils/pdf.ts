ec44ec89c2189ba239a34c4a2e32755a5cc671ea
memokey <skytopground@gmail.com>
2025-02-26 21:23:18 -0500

    Updated

--- utils/pdf.ts
+++ utils/pdf.ts
@@ -23,7 +23,7 @@ export async function generatePdf(markdownContent, outputPath) {
           <style>
               /* Resume Container */
             body {
-              max-width: 1200px;
+              max-width: 1400px;
               margin: 0 auto;
               padding: 30px;
               background: #fff;
